cfc277e79f28a0060b62ffa6af2671c0e1b52ed0
Pawel, Ostrowski <92pawelostrowski@gmail.com>
2017-07-17 23:17:38 +0200

    Logging service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,35 @@
 import { Component } from "@angular/core";
+import { UserService } from "./shared/user/user.service";
+import { User } from "./shared/user/user";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private userService: UserService) {
+    this.user = new User();
+  }
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // TODO: Define
+  }
+  signUp() {
+    this.userService.register(this.user);
+  }
+  
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
